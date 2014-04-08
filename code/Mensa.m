a895cc7d3059a02634cac3a4d69f68aa8bdd9cd7
rbissig <rbissig@student.ethz.ch>
2014-04-08 13:33:16 +0200

    code/Mensa.m

--- code/Mensa.m
+++ code/Mensa.m
@@ -1,3 +1,9 @@
 clear all, clc;
 
+%% Agents
+Data.N = 1; % number of people
+Data.number = 1:Data.N; % assign a number to every person
+Data.time = zeros(Data.N,1); % assign a time to every person
+Data.coords=zeros(Data.N,2);
+Data.activation=zeros(Data.N,1);
 
