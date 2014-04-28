c9ecfde3fa74a4a6bb0cc4d45a30eacbf557c378
daniehot <daniehot@student.ethz.ch>
2014-04-28 15:17:24 +0200

    Mol luege öb das do genau funktioniert!

---
+++ code/arrdec.m
@@ -0,0 +1,8 @@
+function Data = arrdec(Data)
+
+% Computes arraval times and makes decision which menu is taken
+% resp. which queue to line in
+
+
+end
+ 
---
+++ code/main.m
@@ -0,0 +1,15 @@
+clear all, clc;
+
+
+
+%Agents generals
+Data.N = 1800;
+Data.number = (1:Data.N);
+Data.coord = zeros(1:Data.N);
+Data.time = zeros(1:Data.N);
+Data.decision = zeros(1:Data.N);
+
+% Arrival and decision
+k = 1;
+l = 2;
+
