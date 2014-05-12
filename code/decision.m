c5993ce7f9b30c9f3081c8fd6e097a7ed31994e9
daniehot <daniehot@student.ethz.ch>
2014-05-12 12:59:17 +0200

    Emol no mit Rand und dä blödi Punkt (80,80) wo si jetzt meide düen

--- code/decision.m
+++ code/decision.m
@@ -1,35 +1,21 @@
-function Data = decision(Data)
-% Generates the chosen meal and therefore the queue the agent lines in.
-% The meal's probability to be chosen is defined by an intervall
-for i = 1:Data.N
-    Data.meal(i) = rand;
-    if Data.meal(i) >= 0 && Data.meal(i) < 0.3
-        Data.meal(i) = 1; % Ell menu
-    end
+function [Data] = decision(Data,Prob_menu,Aktivagent)
+
+
+
+if Data.activ(Aktivagent) == 1 
     
-    if Data.meal(i) >= 0.3 && Data.meal(i) < 0.5
-        Data.meal(i) = 2; % Menu classic
+   aa = rand;
+   
+    if aa<=Prob_menu(1) ;
+                Data.menu(Aktivagent)=1;
+    elseif aa>Prob_menu(1) && Prob_menu(2)>=aa
+                Data.menu(Aktivagent)=2; 
+    elseif aa>Prob_menu(2) && Prob_menu(3)>=aa
+                Data.menu(Aktivagent)=3; 
+    elseif aa>Prob_menu(3) && Prob_menu(4)>=aa
+                Data.menu(Aktivagent)=4; 
+                
     end
-    
-    if Data.meal(i) >= 0.5 && Data.meal(i) < 0.6
-        Data.meal(i) = 3; % vegi
-    end
-    
-    if Data.meal(i) >= 0.6 && Data.meal(i) < 0.7
-        Data.meal(i) = 4; % Pot and Spoon
-    end
-    
-    
-    if Data.meal(i) >= 0.7 && Data.meal(i) < 0.9
-        Data.meal(i) = 5; % Pasta
-    end
-    
-    if Data.meal(i) >= 0.9 && Data.meal(i) < 0.95
-        Data.meal(i) = 6; % Ala carte
-    end
-    
-    if Data.meal(i) >= 0.95 && Data.meal(i) < 1
-        Data.meal(i) = 7; % salad
-    end
+end
 end
  
--- code/decision2.m
+++ code/decision2.m
@@ -1,30 +1,14 @@
-function [Data] = decision2(Data,Prob_menu,Prob_cash,i)
-
-
-if Data.activ(i) == 1 
+function [Data] = decision2(Data,Prob_cash,i)
     
-   aa = rand;
-   
-    if aa<=Prob_menu(1) ;
-                Data.menu(i)=1;
-    elseif aa>Prob_menu(1) && Prob_menu(2)>=aa
-                Data.menu(i)=2; 
-    elseif aa>Prob_menu(2) && Prob_menu(3)>=aa
-                Data.menu(i)=3; 
-    elseif aa>Prob_menu(3) && Prob_menu(4)>=aa
-                Data.menu(i)=4; 
-                
-    end
-    
-elseif Data.activ(i) == 2
+if Data.activ(i) == 2
     
     bb = rand(1);
     if bb <= Prob_cash(1)
-        Data.cash(i)=1;
+        Data.cash(i)=5;
     elseif bb>Prob_cash(1) && bb<=Prob_cash(2)
-        Data.cash(i)=2;
+        Data.cash(i)=6;
     elseif bb>Prob_cash(2) && bb <= Prob_cash(3)
-        Data.cash(i) =3;
+        Data.cash(i)=7;
     end
 end
 end
